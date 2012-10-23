51170d7f0bfde69a43b0fc4d21355a923f4cf5da
Stephen Webber <swebber@mumble-39.cs.wisc.edu>
2012-10-22 20:55:39 -0500

    BREAK ALL THE THINGS

--- sender/src/main.cpp
+++ sender/src/main.cpp
@@ -11,11 +11,12 @@
 #include <stdio.h>
 #include <vector>
 #include <algorithm>//sort
+#include <istream>//sort
 
 //#include <sys/types.h>
 //#include <sys/socket.h>
 //#include <netinet/in.h>
-//#include <arpa/inet.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 //#include <stdio.h>
 //#include <unistd.h>
@@ -23,7 +24,7 @@
 #include <string.h>
 #include <cstdlib> //strtol
 
-const int MAX_DATA = 1 + 4 + 4 + 5*1024;
+#include "packet.h"
 
 /*
  * TODO:
@@ -31,13 +32,14 @@ const int MAX_DATA = 1 + 4 + 4 + 5*1024;
  */
 int main(int argc, char **argv)
 {
-	/*
-	 * Handle arguments
-	 */
+	// Handle arguments
 
 	// If no commands, do nothing
 	if (argc <= 1)
+	{
+		printf("Please supply arguments.\n");
 		return 0;
+	}
 
 	int cmd;
 
@@ -93,63 +95,183 @@ int main(int argc, char **argv)
 
 	// Verify all required arguments are supplied here
 
+	if (!arg_sender_port)
+	{
+		printf("Please supply a sender port (Usage: -p <port>).\n");
+		return 0;
+	}
+	else if (!arg_requester_port)
+	{
+		printf("Please supply a requester port (Usage: -g <port>).\n");
+		return 0;
+	}
+	else if (!arg_rate)
+	{
+		printf("Please supply a rate (Usage: -r <rate>).\n");
+		return 0;
+	}
+	else if (!arg_seq_no)
+	{
+		printf("Please supply a sequence number (Usage: -q <seq_no>).\n");
+		return 0;
+	}
+	else if (!arg_length)
+	{
+		printf("Please supply a length (Usage: -l <length>).\n");
+		return 0;
+	}
+
 	// Convert arguments to usable form
 
 	unsigned long int sender_port = strtoul(arg_sender_port, NULL, 0);
 	unsigned long int requester_port = strtoul(arg_requester_port, NULL, 0);
+	unsigned long int rate = strtoul(arg_rate, NULL, 0);
+	unsigned long int seq_no = strtoul(arg_seq_no, NULL, 0);
+	unsigned long int length = strtoul(arg_length, NULL, 0);
 
 	// Verify variables are within the correct range
 
 	if (sender_port < 1024 || sender_port > 65536
 			|| requester_port < 1024 || requester_port > 65536)
 	{
-		printf("Please supply a port number between 1025 and 65535.");
-		exit(-1);
+		printf("Please supply port numbers between 1025 and 65535.");
+		return(0);
 	}
 
-	/*
-	 * Initialize the client to request
-	 */
+	// Set up socket connection
 
 	int sock;
-	struct sockaddr_in server_addr;
-	struct hostent *host;
-	char send_data[1024];
+	int bytes_read; // <- note how this is now on its own line!
+	socklen_t addr_len; // <- and this too, with a different type.
+	char recv_data[MAX_DATA];
+	Packet packet;
 
-	if (debug)
-	{
-		host = (struct hostent *) gethostbyname(arg_debug);
-
-		if ((struct hostent *) host == NULL)
-		{
-			printf("Host was not found by the name of %s\n", arg_debug);
-			exit(1);
-		}
-	}
-	else
-		host = (struct hostent *) gethostbyname((char *)"127.0.0.1");
+	struct sockaddr_in requester_addr, sender_addr;
 
 	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 	{
+		perror("Socket");
 		exit(1);
 	}
 
-	// Where we are sending to
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(requester_port);
-	server_addr.sin_addr = *((struct in_addr *)host->h_addr);
-	bzero(&(server_addr.sin_zero),8); // TODO: convert to memset
+	sender_addr.sin_family = AF_INET;
+	sender_addr.sin_port = htons(sender_port);
+	sender_addr.sin_addr.s_addr = INADDR_ANY;
+	bzero(&(sender_addr.sin_zero), 8);
+
+	// Bind port to listen on
+	if (bind(sock, (struct sockaddr *) &sender_addr, sizeof(struct sockaddr)) == -1)
+	{
+		perror("Bind");
+		exit(1);
+	}
+
+	addr_len = sizeof(struct sockaddr);
+
+	// Listen for incoming requests
+
+	printf("Sender waiting for requester on port %ld\n", requester_port);
+	fflush(stdout);
 
 	while (1)
 	{
-		printf("Type Something (q or Q to quit):");
-		gets(send_data);
+		bytes_read = recvfrom(sock, recv_data, sizeof(packet), 0,
+			(struct sockaddr *) &requester_addr, &addr_len);
 
-		if ((strcmp(send_data , "q") == 0) || strcmp(send_data , "Q") == 0)
-			break;
+		Packet recv_packet = recv_data;
 
+		if (recv_packet.type == 'R')
+		{
+			// Break requested file into packets
+
+			std::fstream filestr;
+			filestr.open ((char*)recv_packet.payload, std::fstream::out);
+
+			/*
+			 * CONTAIN IN WHILE LOOP
+			 */
+			Packet send_packet;
+			send_packet.type = 'R';
+			filestr >> send_packet.payload;
+//			send_packet.length =
+//			if (debug)
+//				printf("Packet length: %d", packet.length);
+
+//			sendto(sock, send_data, packet.length + MAX_HEADER, 0,
+//					(struct sockaddr *)&requester_addr, sizeof(struct sockaddr));
+			/*
+			 * CONTAIN IN WHILE LOOP
+			 */
+
+
+			filestr.close();
+
+			// Send a reply
+
+
+
+
+//			sendto(sock, send_data, sizeof(send_data), 0,
+//					(struct sockaddr *)&requester_addr, sizeof(struct sockaddr));
+		}
 		else
-			sendto(sock, send_data, strlen(send_data), 0,
-					(struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+		{
+			// Drop packet
+		}
 	}
+
+	//std::vector<Packet> packets = std::vector<Packet>();
+	//Packet packet = recv_data;
+	//packets.push_back(packet);
+
+//	/*
+//	 * Initialize the client to request
+//	 */
+
+	//	server_addr.sin_family = AF_INET;
+	//	server_addr.sin_port = htons(port);
+	//	server_addr.sin_addr.s_addr = INADDR_ANY;
+	//	bzero(&(server_addr.sin_zero), 8);
+
+//	int sock;
+//	struct sockaddr_in server_addr;
+//	struct hostent *host;
+//	char send_data[1024];
+//
+//	if (debug)
+//	{
+//		host = (struct hostent *) gethostbyname(arg_debug);
+//
+//		if ((struct hostent *) host == NULL)
+//		{
+//			printf("Host was not found by the name of %s\n", arg_debug);
+//			exit(1);
+//		}
+//	}
+//	else
+//		host = (struct hostent *) gethostbyname((char *)"127.0.0.1");
+//
+//	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+//	{
+//		exit(1);
+//	}
+//
+//	// Where we are sending to
+//	server_addr.sin_family = AF_INET;
+//	server_addr.sin_port = htons(requester_port);
+//	server_addr.sin_addr = *((struct in_addr *)host->h_addr);
+//	bzero(&(server_addr.sin_zero),8); // TODO: convert to memset
+//
+//	while (1)
+//	{
+//		printf("Type Something (q or Q to quit):");
+//		gets(send_data);
+//
+//		if ((strcmp(send_data , "q") == 0) || strcmp(send_data , "Q") == 0)
+//			break;
+//
+//		else
+//			sendto(sock, send_data, strlen(send_data), 0,
+//					(struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+//	}
 }
